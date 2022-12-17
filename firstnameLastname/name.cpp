e6e29686ee292e85acd8749dced3b5f96ee41f68
krishnadev7 <krishnadevv12@gmail.com>
2022-12-17 23:50:33 +0530

    2nd commit: learned String data type

---
+++ firstnameLastname/name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string first_name, last_name;
+    cout<<"Enter your firstname"<<endl;
+    cin>>first_name;
+    cout<<"Enter your lastname"<<endl;
+    cin>>last_name;
+    cout<<"Welcome "<<first_name<<" "<<last_name;
+    return 0;
+}
