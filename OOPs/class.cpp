787ca99bc1bb62e844acd7f7392ff6381be669d4
krishnadev7 <krishnadevv12@gmail.com>
2022-12-23 07:05:28 +0530

    14th commit: getter and setter in class

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -13,6 +13,14 @@ public:
     {
         cout << "Your name is: " << name << endl;
     }
+
+    void setSecret(const int & newSecret){
+        secret = newSecret;
+    }
+
+    int getSecret(){
+        return secret;
+    }
 };
 
 int main()
@@ -20,4 +28,6 @@ int main()
     User krish;
     krish.name = "Krishnadev";
     krish.getName();
+    krish.setSecret(42);
+    cout << krish.getSecret()<< endl;
 }
