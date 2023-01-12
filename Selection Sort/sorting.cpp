0fec78daaad42b64485619244c30dd8717b6de51
krishnadev7 <krishnadevv12@gmail.com>
2023-01-12 21:20:37 +0530

    37th commit: Selection sort algorithm

---
+++ Selection Sort/sorting.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int a[], int s)
+{
+    for (int i = 0; i <= s - 2; i++)
+    {
+        int smallest = i;
+        for (int j = i + 1; j <= s - 1; j++)
+        {
+            if (a[j] < a[smallest])
+            {
+                smallest = j;
+            }
+        }
+        swap(a[i], a[smallest]);
+    }
+    cout<<"After Selection Sort"<<endl;
+    for(int i = 0; i<s; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+int main()
+{
+    int s;
+    cout << "Enter Size of the array: " << endl;
+    cin >> s;
+
+    int a[s];
+    cout << "Enter " << s << " elements to the array: " << endl;
+    for (int i = 0; i < s; i++)
+    {
+        cin >> a[i];
+    }
+    selectionSort(a, s);
+    return 0;
+}
