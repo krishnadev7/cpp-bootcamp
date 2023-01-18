3aa1411518faf461f0dcfb3f46c115550c399a32
krishnadev7 <krishnadevv12@gmail.com>
2023-01-18 07:12:33 +0530

    43rd commit: power of n

---
+++ Recursion-1/Power of n/Power.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int power(int x , int n){
+    if(n == 0){
+        return 1;
+    }
+    int smallOutput = power(x,n-1);
+    return x*smallOutput;
+}
+
+int main(){
+    cout<<power(5,3);
+}
