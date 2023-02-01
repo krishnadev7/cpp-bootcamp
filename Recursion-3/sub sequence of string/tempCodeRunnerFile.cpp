d278853e2a4cb63e1a6dc5ae37b40733f54771d2
krishnadev7 <krishnadevv12@gmail.com>
2023-02-01 23:30:18 +0530

    55th commit: merge sort

---
+++ Recursion-3/sub sequence of string/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    print_subs(input.substr(1),output + input[0]);
---
+++ mergeSort/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+while(i<=mid){
+        a[k] = x[i];
+        k++;
+        i++;
+    }
+    while (j<=e)
+    {
+        a[k] = y[j];
+        k++;
+        j++;
+    }
